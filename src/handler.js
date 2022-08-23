5235be29be2b1bf961aff6fd9a4c9188649181f8
hasan <hasanbrd@gmail.com>
2022-08-23 10:39:41 +0700

    add getbooksbyidhandler

--- src/handler.js
+++ src/handler.js
@@ -69,6 +69,28 @@ const getAllBooksHandler = () => ({
     },
 });
 
+const getBooksByIdHandler = (request, h) => {
+    const { id } = request.params;
+
+    const book = books.filter((n) => n.id === id)[0];
+
+    if (book !== undefined) {
+        return {
+            status: 'success',
+            data: {
+                book,
+            },
+        };
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Buku tidak ditemukan',
+    });
+    response.code(404);
+    return response;
+};
+
 module.exports = {
-    addBookHandler, getAllBooksHandler,
+    addBookHandler, getAllBooksHandler, getBooksByIdHandler,
 };
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-const { addBookHandler, getAllBooksHandler } = require("./handler");
+const { addBookHandler, getAllBooksHandler, getBooksByIdHandler } = require("./handler");
 
 const routes = [
     {
@@ -10,6 +10,11 @@ const routes = [
         method: 'GET',
         path: '/books',
         handler: getAllBooksHandler,
+    },
+    {
+        method: 'GET',
+        path: '/books/{id}',
+        handler: getBooksByIdHandler,
     },
 ];
 
